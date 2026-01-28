675efe4dc7f1926b97f31203e30bcadaa5fae215
27841359 <27841359@sun.ac.za>
2026-01-28 11:39:53 +0200

    logo

--- site/components/Logo.tsx
+++ site/components/Logo.tsx
@@ -1,14 +1,12 @@
+
 import React from 'react';
 
 export const Logo = ({ className }: { className?: string }) => (
   <div className={`flex flex-col items-center ${className}`}>
-    <div className="text-5xl font-extralight tracking-tighter text-gray-800 lowercase leading-none" style={{ fontFamily: 'serif' }}>
-      decorama
-    </div>
-    <div className="flex items-center w-full mt-[-4px]">
-      <div className="h-[2px] bg-gray-400 flex-grow mr-2"></div>
-      <span className="text-sm italic font-serif tracking-widest text-gray-600">designers</span>
-      <div className="h-[1px] bg-gray-300 flex-grow ml-2"></div>
-    </div>
+    <img
+      src="/decoramalogo.png"
+      alt="Decorama logo"
+      className="h-18 w-auto mb-1"
+    />
   </div>
 );
